ccc4aa51dcc11f7d09dd839852b1b21021c72241
Дмитрий Богданов <64403847+DmitriyBogdanov@users.noreply.github.com>
2022-08-13 18:45:32 +0300

    test

--- main.cpp
+++ main.cpp
@@ -1,52 +1,93 @@
 #include <iostream>
 
-bool checkEmail = false;
+int checkEmail = 0;
+bool qwer = false;
 std::string email;
-std::string symbols = "!#$%&'*+-/=?^_`{|}~.1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+std::string symbolsLeft = "!#$%&'*+-/=?^_`{|}~.1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+std::string symbolsRight = "-.1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-/*int validatingEmail (std::string qwq) {
-    std::cout << "CHECK " << qwq << std::endl;
-    checkEmail = true;
-    return 0;
-}*/
+std::string separatesPartEmail (std::string email, int part) {
+    std::string result, partLeft, partRight;
+    partLeft = email;
+    partRight = email;
 
-std::string separatesPartEmail_1 (std::string email) {
-    std::string partEmail_1;
-    std::string partEmail_2;
+    partLeft = partLeft.erase(email.find('@'));
+    partRight = partRight.erase(0, email.find('@') + 1);
 
-    for(int i = 0; i < email.length(); i++) {
-        if (email[i] != '@') {
-            partEmail_1 += email[i];
-        } else partEmail_2 = partEmail_1;
-    }
-    //std::cout << "P2 " << partEmail_2;
-    return (partEmail_2);
+    return part == 1 ? result = partLeft : result = partRight;
 }
 
-std::string separatesPartEmail_2 (std::string email) {
-    std::string partEmail_2;
+std::string validatingEmail (std::string email) {
+    std::string partLeft, partRight;
+    partLeft = separatesPartEmail(email, 1);
+    partRight = separatesPartEmail(email, 2);
 
-    for(int i = 0; i < email.length(); i++) {
-        if (email[i] == '@') partEmail_2 += email[i+1];
+    if (partLeft.length() < 64 && partLeft.length() > 1 && partRight.length() < 63 && partRight.length() > 1) {
+        checkEmail = checkEmail + 1;
+    } else checkEmail = 0;
+    std::cout << checkEmail << std::endl;
+
+    std::cout << "CHECKEMAIL " << checkEmail << std::endl;
+
+    int chk = 0;
+
+    for (int i = 0; i < partLeft.length(); i++) {
+
+        //if (qwer) break;
+        for (int k = 0; k < symbolsLeft.length(); k++) {
+            if (partLeft[i] != symbolsLeft[k]) {
+                //checkEmail = 0;
+                //std::cout << symbolsLeft[k] << "|" << std::endl;
+            } else {
+                chk +=1 ;
+                //checkEmail = checkEmail + 1;
+                //qwer = true;
+                //break;
+                //std::cout << "***" << std::endl;
+            }
+        }
+    std::cout << "CHK " << chk << std::endl;
+    std::cout << "CHECKEMAIL " << checkEmail << std::endl;
+    if (chk == partLeft.length()) checkEmail = checkEmail + 1;
+
+        //std::cout << checkEmail << std::endl;
+
+        if (partLeft[i] == '.' && partLeft[i+1] == '.') checkEmail = 0;
+            else checkEmail = checkEmail + 1;
     }
-    return (partEmail_2);
+    /*std::cout << checkEmail << std::endl;/*
+    for (int i = 0; i < partRight.length(); i++) {
+        if (partLeft[i] == symbolsRight[i]) checkEmail = checkEmail + 1;
+            else checkEmail = 0;
+        if (partRight[i] != '.' && partRight[i+1] != '.') checkEmail = checkEmail + 1;
+            else checkEmail = 0;
+    }
+    std::cout << checkEmail << std::endl;
+    if (partLeft[0] != '.' && partLeft[partLeft.length()] != '.') {
+        checkEmail = checkEmail + 1;
+    } else checkEmail = 0;
+    std::cout << checkEmail << std::endl;
+    // проверить длинну строки +
+    // проверить символы +
+    // проверить точку в начале и конце +
+    // две точки подряд
+    // проверить две собаки
+
+    /*if (checkEmail) std::cout << "YES";
+        else std::cout << "NO";*/
+
+    return checkEmail == 3 ? "YES" : "NO";
 }
+
+
 
 int main() {
     std::cout << "Checking the correctness of the email address!" << std::endl << std::endl;
 
-
-
     std::cout << "Enter email address:" << std::endl;
     std::cin >> email;
 
-    //validatingEmail(email);
+    std::cout << validatingEmail(email);
 
-    std::cout << "Part_1 " << separatesPartEmail_1(email) << std::endl;
-    std::cout << "Part_2 " << separatesPartEmail_2(email);
-
-    /*if (checkEmail) std::cout << "Yes";
-        else std::cout << "No";
-    return 0;*/
     std::cout << std::endl << "END";
 }
